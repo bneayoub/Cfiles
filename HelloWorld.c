1b7b81f58f692479602e736dbc88dbecc758cb13
bneayoub <bne.ayoub@gmail.com>
2023-03-21 04:57:20 +0000

    All good

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello World11!\n1\t2\t3\nEnjoy Your day!");
-  
-  return 0;
-
+int main()
+{
+	int i;
+	int n;
+	printf("Hello World11!\n1\t2\t3\nEnjoy Your day!");
+	i = 0;
+	if (n > 0)
+	{
+		printf("hi");
+	}
+	return 0;
 }
