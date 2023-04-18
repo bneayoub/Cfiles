fd7a102cc25a4186cc4e9b0b3a3ad5850c0766d6
bneayoub <bne.ayoub@gmail.com>
2023-04-18 13:31:42 +0000

    print strings

--- 2-print_strings.c
+++ 2-print_strings.c
@@ -3,7 +3,32 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	va_start(args, n);
-	int i=0;	
+    va_list args;
+    va_start(args, n);
+    int i = 0;
+    for (i = 0; i < n; i++)
+    {
+        const char *str = va_arg(args, const char *);
+        if (str == NULL)
+        {
+            printf("(nil)");
+        }
+        else
+        {
+            printf("%s", str);
+        }
+        
+        if (i < n - 1 && separator != NULL)
+        {
+            printf("%s", separator);
+        }
+    }
+    printf("\n");
+    va_end(args);
+}
+
+int main(void)
+{
+ print_strings(", ", 2, "Jay", "Django");
+ return (0);
 }
